27b992322168d030dc850d5f3c9a9d48346b6f2f
kundan7783 <kundan065335@gmail.com>
2026-02-14 10:02:00 +0530

    change status code in auth middleware me

--- middleware/authHandeler.js
+++ middleware/authHandeler.js
@@ -4,13 +4,20 @@ const jwt = require("jsonwebtoken");
 async function verifyAuthToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(" ")[1];
-    if (!token) return res.status(401).json({ message: "Access Denied: No Token Provided" });
+    if (!token) {
+        return res.status(401).json({
+            message: "Access token missing"
+        });
+    }
     try {
         const verified = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
         req.user = verified;         
         next();
     } catch (error) {
-        res.status(400).json({ message: "Invalid or Expired Token" });
+         return res.status(401).json({
+            message: "Access token expired or invalid"
+        });
+       
     }
 }
 
